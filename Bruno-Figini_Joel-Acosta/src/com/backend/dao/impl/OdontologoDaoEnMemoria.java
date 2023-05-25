5d3354a6d4b8c6ad3b28f62758a5482db5739959
BrunoFigini <brunofigini1@gmail.com>
2023-05-25 19:57:40 -0300

    Agregando en Memoria

---
+++ Bruno-Figini_Joel-Acosta/src/com/backend/dao/impl/OdontologoDaoEnMemoria.java
@@ -0,0 +1,38 @@
+package com.backend.dao.impl;
+
+import com.backend.dao.IDao;
+import com.backend.entity.Odontologo;
+import org.apache.log4j.Logger;
+
+import java.util.List;
+
+public class OdontologoDaoMemoria implements IDao<Odontologo> {
+
+    private static final Logger LOGGER = Logger.getLogger(OdontologoDaoMemoria.class);
+
+    private List<Odontologo> odontologoRepositorio;
+
+    public OdontologoDaoMemoria() {
+    }
+
+    public OdontologoDaoMemoria(List<Odontologo> odontologoRepositorio) {
+        this.odontologoRepositorio = odontologoRepositorio;
+    }
+
+
+    @Override
+    public Odontologo guardar( Odontologo odontologo ) {
+        odontologoRepositorio.add(odontologo);
+        LOGGER.info("Odontologo guardado: " + odontologo);
+        return odontologo;
+    }
+
+    @Override
+    public List<Odontologo> listarTodos() {
+        LOGGER.info("Listado de todos los odontologos: \n" + odontologoRepositorio);
+        return odontologoRepositorio;
+    }
+
+
+
+}
